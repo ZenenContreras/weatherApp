13e17341f0520b66c240473a312e2daea930625b
Zenen Contreras Royero <zenencontreras1@gmail.com>
2025-09-28 21:38:25 -0500

    feat: datos de api y funciones  del search input. junto con props para el weatherComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import NavBar from './components/NavBar.jsx'
 import SearchBar from './components/SearchBar.jsx'
 import Weather from './components/Weather.jsx'
+import { useState } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [weatherData, setWeatherData] = useState(null)
+
+  const handleCitySelect = async (city) =>{
+    console.log("Ciudad seleccionada: ", city)
+    setIsLoading(true)
+
+    try{
+      //llamada a la api
+      const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`)
+
+      //guardamos los datos
+      const data = await response.json()
+      console.log("Datos del clima: ", data)
+
+      //guardamos el estado del clima
+      setWeatherData({
+        ...data,
+        location: {
+          name: city.name,
+          country: city.country
+        }
+      })
+
+    }catch(error){
+      console.error("Error al obtener el clima:  ", error)
+    }finally{
+      setIsLoading(false)
+    }
+
+  }
 
   return (
     <div className='h-full bg-[#02012C] md:px-[70px] lg:px-[240px] '>
       <NavBar/>
-      <SearchBar />
-      <Weather />
+      <SearchBar onCitySelect={handleCitySelect} />
+      <Weather weatherData={weatherData} isLoading={isLoading} />
     </div>
     
   )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,55 @@
 import { useState } from "react"
 
-function SearchBar  (){
+function SearchBar ({ onCitySelect }){
     const [isOpen, setIsOpen] = useState(false)
+    const [searchTerm, setSearchTerm] = useState("") //lo que escribe el usuario
+    const [cities, setCities] = useState([]) //ciudades encontras
+    const [isLoading, setIsLoading] = useState(false) //si esta buscando una ciudad
 
-const cities = [{"name": "Lisboa", "country" : "Portugal"},{"name": "Lisboa", "country" : "Portugal"},{"name": "Lisboa", "country" : "Portugal"},{"name": "Lisboa", "country" : "Portugal"}]
+    const searchCities = async (query) =>{
+        //validar si el usario escribio mas de 3 letras
+        if (query.length <3){
+            setCities([])
+            return
+        }
+
+        setIsLoading(true)
+
+        try{
+            //llamada a la API de geocoding para buscar una ciudad con su longitud y latitud
+            const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(query)}&count=5&language=en&format=json`)
+            const data = await response.json()
+            console.log("Ciudades encontradas: ", data)
+
+            // si no hay resultados guardamos un string vacio
+            setCities(data.results || [])
+        }catch (error){
+            console.error("Error al buscar ciudades: ", error)
+            setCities([])
+
+        }finally{
+            setIsLoading(false)
+        }
+
+    }
+
+    // FUNCIÓN: Se ejecuta cada vez que el usuario escribe
+    const handleInputChange = (e) => {
+        const value = e.target.value
+        setSearchTerm(value) // Actualizamos lo que se muestra en el input
+        searchCities(value) // Buscamos ciudades
+        setIsOpen(true) // Abrimos el dropdown
+    }
+
+    // FUNCIÓN: Se ejecuta cuando el usuario hace clic en una ciudad
+    const handleCitySelect = (city) => {
+        setSearchTerm(`${city.name}, ${city.country}`) // Mostramos la ciudad en el input
+        setIsOpen(false) // Cerramos el dropdown
+        onCitySelect(city) // ¡IMPORTANTE! Llamamos la función del padre
+    }
+
+
+
     return (
         <div className=" font-dm pt-20 flex flex-col items-center mx-4">
             <h1 className="font-bold text-white text-5xl text-center">How’s the sky looking today?</h1>
@@ -13,19 +59,32 @@ const cities = [{"name": "Lisboa", "country" : "Portugal"},{"name": "Lisboa", "c
                             className="h-12 bg-[#262540] w-full lg:w-140 rounded-xl pl-12 pr-5 placeholder-white"
                             type="text"
                             placeholder="Search for a place..."
-                            onClick={()=>setIsOpen(!isOpen)}
+                            onChange={handleInputChange}
+                            onFocus={()=> setIsOpen(true)}
                         />
                         <button className="bg-[#2B1B9C] h-12 rounded-xl w-full lg:w-26">
                             Search
-                        </button>
+                        </button>  
                 </div>
             {isOpen && (
                 <div className="absolute top-full lg:top-[75%] right-0 w-full bg-[#262540] border border-gray-600 rounded-lg shadow-xl z-50 p-2 text-white font-sans flex flex-col items-center ">
-                    {cities.map((cities)=>{
-                        return (
-                            <button className="p-4 rounded-lg w-full text-left">{cities.name}, {cities.country}</button>
-                        )
-                    })}
+                        {isLoading ? (
+                            <div class="animate-spin inline-block size-6 border-4 border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading">
+                            </div>
+                        ) : cities.length > 0 ? (
+                            cities.map((city, index) => (
+                                <button
+                                    key={index}
+                                    className="p-4 rounded-lg w-full text-left hover:bg-[#3C3B5E] transition-colors"
+                                    onClick={()=> handleCitySelect(city)}
+                                >
+                                    {city.name}, {city.country}
+                                    {city.admin1 && `, ${city.admin1}`}
+                                </button>
+                            ))
+                        ): searchTerm.length > 1 ? (
+                            <div className="p-4 text-gray-400">No cities Founded</div>
+                        ) : null}
                 </div>
             )}
             </div>
